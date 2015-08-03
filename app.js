d3e86aab0fb97256b8f2f33239bd602ddb2fef21
LYLLLRH <lylllrh@163.com>
2015-08-03 14:33:15 +0800

    Add Etag
    Add Etag

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public'),{maxAge:60*60*1000,hidden:false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'),{maxAge:60*60*1000,hidden:false}));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
